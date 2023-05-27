730c2f8c70434c1acd93b1d3c049c39745c9f029
Aguinaldo Moreira <aguinaldo.moreira@senior.com.br>
2023-05-27 16:35:59 -0300

    remove dist folder

--- dist/middlewares/error.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorMiddleware = void 0;
-const errorMiddleware = (error, req, res, next) => {
-    const statusCode = error.statusCode ?? 500;
-    const message = error.statusCode ? error.message : 'Internal Server Error';
-    return res.status(statusCode).json({ message });
-};
-exports.errorMiddleware = errorMiddleware;
